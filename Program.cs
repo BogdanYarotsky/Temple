fb35038c23a756d18e09987d0669276076d4c80e
BogdanYarotsky <byarotsky@gmail.com>
2024-02-12 21:56:41 +0100

    creating react app using vite

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
-// 1) publish as tool to nuget (version 0.1.0)
+﻿using System.Diagnostics;
+using static System.Console;
+using static System.Text.Json.JsonSerializer;
+using CliWrap;
+using CliWrap.Buffered;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        // check current directory
+        // find csproj - if not found - return
+
+        // if npm not installed - terminate
+        // if vite not installed - install
+        var result = await Cli.Wrap("npm")
+            .WithArguments(new []
+            {
+                "create", "vite@latest", "ui2", "--", "--template", "react-swc-ts"
+            })
+            .ExecuteBufferedAsync();
+
+    }
+}
